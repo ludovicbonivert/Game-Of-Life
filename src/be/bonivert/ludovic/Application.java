a6c904c0a79bf49ff60e1db0b071b2385bbcd2ec
ludovicbonivert <ludovic0_0@hotmail.com>
2014-06-19 15:42:43 +0200

    Implemented a timer and first living cells

--- src/be/bonivert/ludovic/Application.java
+++ src/be/bonivert/ludovic/Application.java
@@ -3,35 +3,80 @@ import java.util.ArrayList;
 import processing.core.*;
 
 /** Game of Life main class **/
-
+/** Timer from learningprocessing.com (Daniel Shiffman) **/
+// Timer is useless at the moment, better without
 public class Application extends PApplet{
-	/** Basic setup :
-	 * -Size of window
-	 * -Background init
-	 * -ArrayList init
-	 * -stroke of lines
-	 * **/
+	
+	//Arraylist must be declared outside setup, else we cant access it
+	ArrayList<Cell> myCells = new ArrayList<Cell>();
+	// Prepare first state of Cells. Randomize is in setup
+	int randomX, randomY;
+	// Timer, refresh every 1000 (1 sec)
+	int refresh = 0;
+	int savedTime;
+	
 	public void setup() {
-	    size(400,400);
+		savedTime = millis();
+		int height = 400;
+		int width = 400;
+	    size(height, width);
 	    background(255);
-	    ArrayList<Cell> myCells = new ArrayList<Cell>();
 	    stroke(0);
+	    
+	    // Making y number of Cells (in this example 40*40 = 1600 Cells
+	    // 1 cell is 10x10
+	    int y = 0;
+	    int numberOfCells = (height * width) / 100;
+	    while(y < numberOfCells){
+	    	myCells.add(new Cell(true));
+	    	y++;
+	    }
+	    println(myCells.size());
+	    
+	    //Making grid in setup, not in draw because it is continuously redrawed
+	    for(int i = 0; i < width; i+=10){
+			  line(0, i, width, i);
+			  line(i, 0, i, height);
+		}
+	 // First green fill of living cells
+	    fill(155, 253, 69);
+	 // Making initial (random) state of Cells
+	 // Search 10 times for a random number between 0 and width length
+	    for(int i = 0; i <= 10; i++){
+	    	randomX = floor(random(0, (width/10)));
+		    randomY = floor(random(0, (height/10)));
+		    // Make this numbers * 10 to fit in grid
+		    randomX *= 10;
+		    randomY *= 10;
+		    rect(randomX, randomY, 10, 10);
+	    }
 	  }
+	
 	/** 
 	 * Draw method : where the magic happens
 	 * **/
 	  public void draw() {
-		  // Making of the grid. One rect is 10x10
-		  for(int i = 0; i < 400; i+=10){
-			  line(0, i, 400, i);
-			  line(i, 0, i, 400);
-		  };
+		  // Calculates how much time has passed
+		  int passedTime = millis() - savedTime;
+		  // Has 1 second passed ? 
+		  if(passedTime > refresh){
+			  for(int i = 0; i <= 10; i++){
+			    	randomX = floor(random(0, (width/10)));
+				    randomY = floor(random(0, (height/10)));
+				    // Make this numbers * 10 to fit in grid
+				    randomX *= 10;
+				    randomY *= 10;
+				    rect(randomX, randomY, 10, 10);
+			    } 
+			  savedTime = millis(); // Save the current time to restart the timer
+		  }
 		  
+		  // Making of the grid. One rect is 10x10
 		  // If the cell is dead give it a red color
-		  fill(253, 69, 69);
-		  rect(80, 70, 10, 10);
+		  //fill(253, 69, 69);
+		  //rect(80, 70, 10, 10);
 		  // If the cell is alive giv it a green color
 		  fill(155, 253, 69);
-		  rect(50, 60, 10, 10);
+		  rect(randomX, randomY, 10, 10);
 	  }
 }
